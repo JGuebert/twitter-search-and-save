522dd3393fd26fd5c628b8e47738471b495f5a4e
Jeff Guebert <jeff@jeffguebert.com>
2019-01-26 10:21:06 -0800

    Initial commit of script and documentation

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # twitter-search-and-save
 PowerShell script that locally saves the JSON responses returned from the Twitter Search API
+
+## Setup
+After cloning the repository, open download.ps1 and fill in the Configuration Variables at the top of the script
+- **$bearertoken**: The access token returned from https://api.twitter.com/oauth2/token (see https://developer.twitter.com/en/docs/basics/authentication/guides/bearer-tokens)
+- **$queryparams**: Your query parameters for the initial query, starting with ?. For example, "?q=nasa&result_type=popular" (see https://developer.twitter.com/en/docs/tweets/search/api-reference/get-search-tweets.html)
+- **$maxrequests**: The maximum number of times you want the script to call the API to get a batch of tweets. Subsequent calls are made with the query parameters returned in the next_results field of search_metadata of the previous response
+
+## Usage
+Run download.ps1 from the directory where you want the output JSON to be saved. The script creates a series of files named "tweets-(incrementing number)-(max_id from the response).json" each containing the JSON output from one query.
---
+++ download.ps1
@@ -0,0 +1,43 @@
+﻿# twitter-search-and-save/download.ps1
+# Version: 0.1
+# License: MIT
+# Website: https://github.com/JGuebert/twitter-search-and-save
+
+# Configuration Variables
+
+$bearertoken = # ADD YOUR BEARER TOKEN HERE
+$queryparams = # ADD YOUR QUERY STRING HERE
+$maxrequests = # SET THE MAXIMUM NUMBER OF REQUESTS TO MAKE
+
+
+
+##### DO NOT MODIFY ANYTHING BELOW THIS LINE #####
+
+
+
+# Initialize script variables
+$count = 0
+$nextquery = $queryparams
+
+$bearerheader = "Bearer " + $bearertoken
+
+Do
+{
+    
+    # Get the data from the API
+    $baseuri = "https://api.twitter.com/1.1/search/tweets.json"
+    $requesturi = $baseuri + $nextquery
+    $response = Invoke-WebRequest -Uri $requesturi -Headers @{Authorization=$bearerheader}
+    
+    # Write the content returned to a file
+    $responsejson = $response.Content | ConvertFrom-Json
+    $filepath = ".\tweets-" + $count + "-" + $responsejson.search_metadata.max_id + ".json"
+    Out-File $filepath -InputObject $response.Content
+
+    #Increment $count for the next time
+    $count++
+
+    # Figure out what the next query needs to be to get the next set of older tweets
+    $nextquery = $responsejson.search_metadata.next_results
+
+} While (($responsejson.statuses.Count -gt 0) -and ($count -lt $maxrequests) -and ($nextquery))
