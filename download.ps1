b8eeb3ef0910d748e8bd364aa534fd3067809ae2
Jeff Guebert <jeff@jeffguebert.com>
2019-04-15 22:14:22 -0700

    Control download behavior with parameters

--- download.ps1
+++ download.ps1
@@ -11,37 +11,51 @@ param (
     [parameter(Mandatory=$true)]   
     [string]
     $QueryString,
+
+    [switch]$ExtendedMode,
+
+    [string]$OutputName,
     
     [parameter(Mandatory=$true)]
     [int]
     $MaxRequests
 );
 
+# Set $ExtendedMode switch if user included tweet_mode=extended in query but did not set use switch when calling script
+If (!$QueryString.Contains("tweet_mode=extended")) { $ExtendedMode = $true }
 
-# If $QueryString does not include tweet_mode, prompt user for if tweet_mode=extended should be used (with default of yes)
+# If $QueryString does not include tweet_mode and $ExtendedMode switch is not set, prompt user for if tweet_mode=extended should be used (with default of yes)
 If (!$QueryString.Contains("tweet_mode")) {
-    $extendedanswer = ""
-    Do {
-        $extendedanswer = Read-Host "Do you want tweets returned in extended mode? (Y/n)"
-        if(!$extendedanswer -or $extendedanswer.Equals("y")) { $extendedanswer = "Y" }
-    } While (!($extendedanswer.Equals("Y") -or $extendedanswer.Equals("N") -or $extendedanswer.Equals("n")))
+    # If $ExtendedMode switch is set, add to query string
+    If ($ExtendedMode) { $QueryString = $QueryString + "&tweet_mode=extended" }
+    # Otherwise need to ask user
+    Else {
+        $extendedanswer = ""
+        Do {
+            $extendedanswer = Read-Host "Do you want tweets returned in extended mode? (Y/n)"
+            if(!$extendedanswer -or $extendedanswer.Equals("y")) { $extendedanswer = "Y" }
+        } While (!($extendedanswer.Equals("Y") -or $extendedanswer.Equals("N") -or $extendedanswer.Equals("n")))
 
-    # If user answered yes, add tweet_mode=extended to the query
-    If($extendedanswer.Equals("Y")) { $QueryString = $QueryString + "&tweet_mode=extended" }
+        # If user answered yes, add tweet_mode=extended to the query
+        If($extendedanswer.Equals("Y")) { 
+            $QueryString = $QueryString + "&tweet_mode=extended"
+            $ExtendedMode = $true
+        }
+    }
 }
-
-while($MaxRequests -lt 1) { [int]$MaxRequests = Read-Host "Max number of API requests" }
 
 
 # Initialize script variables
 $count = 0
 $nextquery = $QueryString
-$extendedmode = If ($QueryString.Contains("tweet_mode=extended")) {$true} Else {$false}
 
 $bearerheader = "Bearer " + $BearerToken
 
+If (!$OutputName) { $OutputName = "tweets" }
+$outputpath = ".\" + $OutputName
+
 # Create directory for output JSON to be stored in, direct output to $null so it doesn't appear in console
-New-Item -ItemType Directory -Force -Path ".\tweets" > $null
+New-Item -ItemType Directory -Force -Path $outputpath > $null
 
 Do
 {
@@ -53,18 +67,18 @@ Do
     
     # Write the content returned to a file
     $responsejson = $response.Content | ConvertFrom-Json
-    $filepath = ".\tweets\tweets-" + $count + "-" + $responsejson.search_metadata.max_id + ".json"
+    $filepath = $outputpath + "\tweets-" + $count + "-" + $responsejson.search_metadata.max_id + ".json"
     Out-File $filepath -InputObject $response.Content
 
     # Increment $count for the next time
     $count++
 
     # Figure out what the next query needs to be to get the next set of older tweets
-    $nextquery = If ($extendedmode) {$responsejson.search_metadata.next_results + "&tweet_mode=extended"} Else {$responsejson.search_metadata.next_results}
+    $nextquery = If ($ExtendedMode) {$responsejson.search_metadata.next_results + "&tweet_mode=extended"} Else {$responsejson.search_metadata.next_results}
 
 } While (($responsejson.statuses.Count -gt 0) -and ($count -lt $MaxRequests) -and ($nextquery))
 
 # Save the query parameters used to a query.txt file
-Out-File ".\tweets\query.txt" -InputObject $QueryString
+Out-File "$outputpath\query.txt" -InputObject $QueryString
 
-Compress-Archive -Path ".\tweets\*" -DestinationPath ".\output.zip"
+Compress-Archive -Path "$outputpath\*" -DestinationPath ".\$OutputName.zip"
