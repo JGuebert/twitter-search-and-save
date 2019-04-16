a778e83c764e63792e80354993b233ec283c48a5
Jeff Guebert <jeff@jeffguebert.com>
2019-04-15 21:57:49 -0700

    Variables are set via parameters

--- download.ps1
+++ download.ps1
@@ -3,24 +3,23 @@
 # License: MIT
 # Website: https://github.com/JGuebert/twitter-search-and-save
 
-# Configuration Variables
+param (
+    [parameter(Mandatory=$true)]    
+    [string]
+    $BearerToken,
+    
+    [parameter(Mandatory=$true)]   
+    [string]
+    $QueryString,
+    
+    [parameter(Mandatory=$true)]
+    [int]
+    $MaxRequests
+);
 
-$bearertoken = "" # ADD YOUR BEARER TOKEN HERE
-$queryparams = "" # ADD YOUR QUERY STRING HERE
-$maxrequests = 0 # SET THE MAXIMUM NUMBER OF REQUESTS TO MAKE
 
-
-
-##### DO NOT MODIFY ANYTHING BELOW THIS LINE #####
-
-
-
-# Prompt user for input if not set in script
-if(!$bearertoken) { $bearertoken = Read-Host "Bearer token" }
-if(!$queryparams) { $queryparams = Read-Host "Query string (starting with ?)" }
-
-# If $queryparams does not include tweet_mode, prompt user for if tweet_mode=extended should be used (with default of yes)
-If (!$queryparams.Contains("tweet_mode")) {
+# If $QueryString does not include tweet_mode, prompt user for if tweet_mode=extended should be used (with default of yes)
+If (!$QueryString.Contains("tweet_mode")) {
     $extendedanswer = ""
     Do {
         $extendedanswer = Read-Host "Do you want tweets returned in extended mode? (Y/n)"
@@ -28,18 +27,18 @@ If (!$queryparams.Contains("tweet_mode")) {
     } While (!($extendedanswer.Equals("Y") -or $extendedanswer.Equals("N") -or $extendedanswer.Equals("n")))
 
     # If user answered yes, add tweet_mode=extended to the query
-    If($extendedanswer.Equals("Y")) { $queryparams = $queryparams + "&tweet_mode=extended" }
+    If($extendedanswer.Equals("Y")) { $QueryString = $QueryString + "&tweet_mode=extended" }
 }
 
-while($maxrequests -lt 1) { [int]$maxrequests = Read-Host "Max number of API requests" }
+while($MaxRequests -lt 1) { [int]$MaxRequests = Read-Host "Max number of API requests" }
 
 
 # Initialize script variables
 $count = 0
-$nextquery = $queryparams
-$extendedmode = If ($queryparams.Contains("tweet_mode=extended")) {$true} Else {$false}
+$nextquery = $QueryString
+$extendedmode = If ($QueryString.Contains("tweet_mode=extended")) {$true} Else {$false}
 
-$bearerheader = "Bearer " + $bearertoken
+$bearerheader = "Bearer " + $BearerToken
 
 # Create directory for output JSON to be stored in, direct output to $null so it doesn't appear in console
 New-Item -ItemType Directory -Force -Path ".\tweets" > $null
@@ -63,9 +62,9 @@ Do
     # Figure out what the next query needs to be to get the next set of older tweets
     $nextquery = If ($extendedmode) {$responsejson.search_metadata.next_results + "&tweet_mode=extended"} Else {$responsejson.search_metadata.next_results}
 
-} While (($responsejson.statuses.Count -gt 0) -and ($count -lt $maxrequests) -and ($nextquery))
+} While (($responsejson.statuses.Count -gt 0) -and ($count -lt $MaxRequests) -and ($nextquery))
 
 # Save the query parameters used to a query.txt file
-Out-File ".\tweets\query.txt" -InputObject $queryparams
+Out-File ".\tweets\query.txt" -InputObject $QueryString
 
 Compress-Archive -Path ".\tweets\*" -DestinationPath ".\output.zip"
