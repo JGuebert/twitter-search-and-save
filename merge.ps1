f6b975be094a68a83b4609ed1bf49a5b8b3b3916
Jeff Guebert <jeff@jeffguebert.com>
2022-01-03 20:09:19 -0800

    Adding files to merge branch

---
+++ merge.ps1
@@ -0,0 +1,5 @@
+# twitter-search-and-save/merge.ps1
+# Version: 0.1-merge
+# License: MIT
+# Website: https://github.com/JGuebert/twitter-search-and-save
+
---
+++ test.ps1
@@ -0,0 +1,4 @@
+﻿$bearertoken = "AAAAAAAAAAAAAAAAAAAAAPV09QAAAAAA0eIL7iRNDVLK4Ivnpq2Z4sG1ydA%3DkN5GNtzZK5hvkmSx0XPUWttbxiRh8qCDYzPam63S7nGNd6w1kM"
+$querystring = "?q=nasa"
+
+.\download.ps1 -BearerToken $bearertoken -QueryString $querystring -ExtendedMode -OutputName 2021-nasa -MaxRequests 5
