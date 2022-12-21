c630861e32d5143d2301f5583c0a4a49e29400b5
Jeff Guebert <jeff@jeffguebert.com>
2022-12-20 23:17:53 -0800

    Support path to tweets and printing summary

--- sentiment.ps1
+++ sentiment.ps1
@@ -1,5 +1,5 @@
 # twitter-search-and-save/sentiment.ps1
-# Version: 0.1
+# Version: 0.2-docker-sentiment
 # License: MIT
 # Website: https://github.com/JGuebert/twitter-search-and-save
 
@@ -11,9 +11,15 @@ Param(
     [String[]]
     $Tweets,
 
+    [string]
+    $TweetPath,
+
     [parameter(Mandatory=$true)]
     [int]
     $MinSentiment,
+
+    [switch]
+    $ShowSummary,
 
     [parameter(Mandatory=$false,ParameterSetName="positive")]
     [switch]
@@ -23,10 +29,6 @@ Param(
     [switch]
     $NegativeOnly
 )
-
-# Configuration Variables
-
-##### DO NOT MODIFY ANYTHING BELOW THIS LINE #####
 
 function Get-Sentiment {
     Param($tweettext)
@@ -39,26 +41,36 @@ function Get-Sentiment {
     $sentiment = $response.Content | ConvertFrom-Json
 
     # Only display output if it is above (for positive) or below (for negative) the $MinSentiment value
-    if(($sentiment -lt -$MinSentiment) -and !($PositiveOnly)) {
-        "NEGATIVE Sentiment of " + $sentiment + ": " + $tweettext
+    if(($sentiment -le -$MinSentiment) -and !($PositiveOnly)) {
+        Write-Host "NEGATIVE Sentiment of $sentiment : $tweettext"
     }
 
-    if($sentiment -gt $MinSentiment -and !($NegativeOnly)) {
-        "POSITIVE Sentiment of " + $sentiment + ": " + $tweettext
+    if($sentiment -ge $MinSentiment -and !($NegativeOnly)) {
+        Write-Host "POSITIVE Sentiment of $sentiment : $tweettext"
     }
+
+    $sentiment
 }
+
+$sentimentsummary = @{}
 
 # Use the passed in tweets if given
 if($Tweets)
 {
     foreach($tweettext in $Tweets) {
-        Get-Sentiment $tweettext
+        $sentiment = Get-Sentiment $tweettext
+        if($sentimentsummary.ContainsKey($sentiment)) { $sentimentsummary.$sentiment = $sentimentsummary.$sentiment + 1 } else { $sentimentsummary.$sentiment = 1 }
     }
 }
 # Otherwise look at all of the previously saved tweets
 else {
+    if($TweetPath) {
+        $tweetfiles = Get-ChildItem -Path "$TweetPath\tweets-*.json"
+    }
     # Load the tweets that are present in the /tweets directory
-    $tweetfiles = Get-ChildItem -Path ".\tweets\tweets-*.json"
+    else {
+        $tweetfiles = Get-ChildItem -Path ".\tweets\tweets-*.json"
+    }
 
     foreach($file in $tweetfiles) {
 
@@ -79,9 +91,32 @@ else {
                 else {
                     $tweettext = $status.text
                 }
-                Get-Sentiment $tweettext
+                $sentiment = Get-Sentiment $tweettext
+                if($sentimentsummary.ContainsKey($sentiment)) { $sentimentsummary.$sentiment = $sentimentsummary.$sentiment + 1 } else { $sentimentsummary.$sentiment = 1 }
             }
         }
 
     }
+}
+
+if($ShowSummary) { 
+    $sentimentscores = $sentimentsummary.Keys | Sort-Object
+
+    $sentimentbuckets = @{}
+    # Sentiment buckets:
+    $sentimentbuckets.strongneg = 0 # Strong negative: -6 or less
+    $sentimentbuckets.weakneg = 0 # Weak negative: -5 <-> -3 
+    $sentimentbuckets.neutral = 0 # Netural: -2 <-> 2
+    $sentimentbuckets.weakpos = 0 # Weak positive: 3 <-> 5
+    $sentimentbuckets.strongpos = 0 # Strong positive: 6 or more
+
+    foreach($score in $sentimentscores) {
+        if($score -le -6) { $sentimentbuckets.strongneg += $sentimentsummary.$score }
+        elseif($score -ge -5 -and $score -le -3) { $sentimentbuckets.weakneg += $sentimentsummary.$score }
+        elseif($score -ge -2 -and $score -le 2) { $sentimentbuckets.neutral += $sentimentsummary.$score }
+        elseif($score -ge 3 -and $score -le 5) { $sentimentbuckets.weakpos += $sentimentsummary.$score }
+        else { $sentimentbuckets.strongpos += $sentimentsummary.$score }
+    }
+
+    $sentimentbuckets
 }
